e8bcafa754f590876115301b3f7c64692cf97a59
anuragmaganti <amaganti.dev@gmail.com>
2026-04-28 20:10:46 -0400

    add state to track and respond to mouse hover over profile button

--- src/components/header/aboutMe.jsx
+++ src/components/header/aboutMe.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { Triangle } from "lucide-react";
 
 const AboutMe = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex items-center gap-2.5">
-      <div>
-        <img src="me.png" alt="" className="max-w-7.5 rounded-sm" />
-      </div>
-      <Triangle width={9} fill="white" className="rotate-180" strokeWidth={0} />
+    <div
+      className="relative"
+      onMouseEnter={() => setIsOpen(!isOpen)}
+      onMouseLeave={() => setIsOpen(!isOpen)}
+    >
+      <button
+        type="button"
+        className="flex items-center gap-2.5 cursor-pointer"
+      >
+        <img src="me.png" alt="Profile" className="max-w-7.5 rounded-sm" />
+        <Triangle
+          width={9}
+          fill="white"
+          className={`duration-200 ${isOpen ? "rotate-0" : "rotate-180"}`}
+          strokeWidth={0}
+        />
+      </button>
+      {isOpen && (
+        <div className="absolute">
+          <li>hi</li>
+        </div>
+      )}
     </div>
   );
 };

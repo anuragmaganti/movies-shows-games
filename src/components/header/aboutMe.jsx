fcfd325037d18e04e11c82f3265c126abba9f8fb
anuragmaganti <amaganti.dev@gmail.com>
2026-04-29 13:16:58 -0400

    add hover styling to menu items

--- src/components/header/aboutMe.jsx
+++ src/components/header/aboutMe.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Triangle, FolderHeart, Code } from "lucide-react";
 
 const AboutMe = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const closeTimeout = useRef(null);
+
+  function closeMenuDelay() {
+    closeTimeout.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 200);
+  }
+
+  function openMenu() {
+    if (closeTimeout.current) {
+      clearTimeout(closeTimeout.current);
+    }
+    setIsOpen(true);
+  }
 
   return (
     <div
       className="relative"
-      onMouseEnter={() => setIsOpen(!isOpen)}
-      onMouseLeave={() => setIsOpen(!isOpen)}
+      onMouseEnter={openMenu}
+      onMouseLeave={closeMenuDelay}
     >
       <button
         type="button"
@@ -23,17 +37,20 @@ const AboutMe = () => {
         />
       </button>
       {isOpen && (
-        <div className="absolute right-0 top-full w-45 p-3 flex flex-col gap-2 border border-gray-700 ">
-          <a className="flex gap-3 pl-2" href="https://www.amaganti.com/">
+        <div className="absolute mt-3 right-0 top-full w-45 p-3 flex flex-col gap-2 border border-gray-800">
+          <a
+            className="group flex gap-3 pl-2 "
+            href="https://www.amaganti.com/"
+          >
             <FolderHeart></FolderHeart>
-            <div>Portfolio site</div>
+            <div className="group-hover:underline">Portfolio site</div>
           </a>
           <a
-            className="flex gap-3 pl-2"
+            className="group flex gap-3 pl-2"
             href="https://github.com/anuragmaganti"
           >
             <Code></Code>
-            <div>Github</div>
+            <div className="group-hover:underline">Github</div>
           </a>
         </div>
       )}

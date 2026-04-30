9c57765da77fc0ac720378faa772e40778e878fb
anuragmaganti <amaganti.dev@gmail.com>
2026-04-30 13:48:06 -0400

    restructe project into frontend and backend directories

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./components/header-components/header.jsx";
+import MoviePage from "./pages/MoviePage.jsx";
+import VideoGamePage from "./pages/VideoGamePage.jsx";
+import TvShowPage from "./pages/TvShowPage.jsx";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MoviePage />}></Route>
+        <Route path="/tv-shows" element={<TvShowPage />}></Route>
+        <Route path="/video-games" element={<VideoGamePage />}></Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;

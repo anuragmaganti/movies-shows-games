6d0401a897400e933fcc946825df9d99d963dab2
anuragmaganti <amaganti.dev@gmail.com>
2026-04-30 18:21:59 -0400

    fix npm script mismatch

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const PORT = 3000;
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port: ${PORT}`);
+});
